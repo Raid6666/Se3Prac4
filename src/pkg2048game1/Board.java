58c97bcc82998c69a8a05b9a37f23ce869311e86
raedalmutairi <raedalmutairi@192.168.1.101>
2014-06-01 17:37:47 +0300

    - Create displayBoard method

--- src/pkg2048game1/Board.java
+++ src/pkg2048game1/Board.java
@@ -8,6 +8,36 @@ package pkg2048game1;
  *
  * @author raedalmutairi
  */
+
 public class Board {
     
+    private Tile[][] tiles= new Tile[4][4];
+    
+    public Board(){
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                tiles[i][j] = new Tile(0);
+            }
+        }
+    }
+    
+    public void displayBoard(){
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                if(tiles[i][j].getValue()<10){
+                    System.out.print("[   "+tiles[i][j]+"]");
+                }
+                else if(tiles[i][j].getValue()<100){
+                    System.out.print("[  "+tiles[i][j]+"]");
+                }
+                else if(tiles[i][j].getValue()<1000){
+                    System.out.print("[ "+tiles[i][j]+"]");
+                }
+                else if(tiles[i][j].getValue()<10000){
+                    System.out.print("["+tiles[i][j]+"]");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
 }
