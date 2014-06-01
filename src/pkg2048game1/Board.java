81911adee9823bfd6bf834e1b2526932dbb6fe68
raedalmutairi <raedalmutairi@192.168.1.101>
2014-06-01 17:12:38 +0300

    - Create Board , Tile classes and Create Run class as main class.

---
+++ src/pkg2048game1/Board.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2048game1;
+
+/**
+ *
+ * @author raedalmutairi
+ */
+public class Board {
+    
+}
