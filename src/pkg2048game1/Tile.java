b453f663473390635f43d679e54da6673f6a5588
raedalmutairi <raedalmutairi@192.168.1.101>
2014-06-01 17:42:10 +0300

    - add Tile class body that includes adding necessary variables and methods

--- src/pkg2048game1/Tile.java
+++ src/pkg2048game1/Tile.java
@@ -8,6 +8,32 @@ package pkg2048game1;
  *
  * @author raedalmutairi
  */
+
 public class Tile {
+    
+    private int value;
+
+    public Tile(int value){
+        this.value = value;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        if (value == 0){
+            return "";
+        }
+        else{
+            return "" +  value ;
+        }
+    }
+    
     
 }
